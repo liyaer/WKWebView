aa13f14b895757d1488290a8646c0d009f108a10
Company_DuBenben <2249923489@qq.com>
2020-08-20 17:10:42 +0800

    完善 baseURL 参数测试，更新注释

--- WKWebView/BaseURL参数测试/BaseURLTestVC.m
+++ WKWebView/BaseURL参数测试/BaseURLTestVC.m
@@ -10,7 +10,7 @@
 #import <WebKit/WebKit.h>
 
 
-#define cese 1
+#define case 2
 
 
 @interface BaseURLTestVC ()
@@ -19,10 +19,8 @@
 
 
 /*
-    首先，要了解 黄色文件夹 和 蓝色文件夹 的区别：
-        黄色：
-        蓝色：
- 算了，做成笔记吧！
+    首先，要了解 黄色文件夹 和 蓝色文件夹 的区别！
+    算了，做成笔记吧！
  */
 @implementation BaseURLTestVC
 
@@ -41,54 +39,65 @@
     //测试前提：文件内引用资源使用的是相对路径
 
 //----------------------------------常规情况------------------------------------
-#if (case == 1 || case == 2)
-    //case1：文件、资源都在黄色文件夹，且在同一级目录。baseURL 传 [[NSBundle mainBundle] bundleURL]] 即可（传 [NSURL fileURLWithPath:localPath]也可以）
-    //case2：文件、资源都在黄色文件夹，但不在同一级目录。同case1
-/*  错误的访问方式，localPath = null
+    
+#if (case == 1)
+    //case1：文件、资源都在黄色文件夹，是否在同一级目录无所谓
+/*
+ *错误的访问方式，localPath = null
     localPath = [[NSBundle mainBundle] pathForResource:@"index1" ofType:@"html" inDirectory:@"Resources"];
     NSLog(@"%@", localPath);
     localPath = [[NSBundle mainBundle] pathForResource:@"index1" ofType:@"html" inDirectory:[[NSBundle mainBundle] bundlePath]];
     NSLog(@"%@", localPath);
  */
-    //文件在黄，通过该方法，使用 相对路径 访问
+    //正确的访问方式，文件在黄，通过该方法，使用 相对路径 访问
     localPath = [[NSBundle mainBundle] pathForResource:@"index1" ofType:@"html"];
     NSLog(@"%@", localPath);
-#elif (case == 3)
-    //case3：文件、资源都在蓝色文件夹，且在同一级目录。baseURL 传 [NSURL fileURLWithPath:localPath]（文件路径） 即可
-/*  错误的访问方式，localPath = null
+#elif (case == 2)
+    //case2：文件、资源都在蓝色文件夹，且在同一级目录
+/*
+ *错误的访问方式，localPath = null
     localPath = [[NSBundle mainBundle] pathForResource:@"index2" ofType:@"html"];
     NSLog(@"%@", localPath);
  */
-    //文件在蓝，通过下面方法二选一，使用 绝对路径 访问
-    localPath = [[NSBundle mainBundle] pathForResource:@"ReadHtml/index2" ofType:@"html"];
-    NSLog(@"%@", localPath);
-//    localPath = [[NSBundle mainBundle] pathForResource:@"index2" ofType:@"html" inDirectory:@"ReadHtml"];
+    //正确的访问方式，文件在蓝，通过下面方法二选一，使用 绝对路径 访问
+//    localPath = [[NSBundle mainBundle] pathForResource:@"ReadHtml/index2" ofType:@"html"];
 //    NSLog(@"%@", localPath);
+    localPath = [[NSBundle mainBundle] pathForResource:@"index2" ofType:@"html" inDirectory:@"ReadHtml"];
+    NSLog(@"%@", localPath);
 #endif
+    
+/*
+ *allowingReadAccessToURL 和 baseUrl一样，代表html内引用的资源所在的目录地址
+ *无论html文件内引用的资源是处在黄色还是蓝色文件夹，也无论各个资源所处的目录层级，最终都会被copy到mainBundle（关于这一点可以看笔记，蓝色：整体copy 黄色：仅copy资源文件），这就是为什么 BaseURL说明.png 中说一般传mainBundle路径即可
+ *当然如果你明确资源的位置，可以适当缩小范围，不传mainBundle路径，而是像下面这样
+    [webView loadFileURL:fileUrl allowingReadAccessToURL:[fileUrl URLByDeletingLastPathComponent]];
+  甚至可以直接将fileUrl传进去
+ */
     fileUrl = [NSURL fileURLWithPath:localPath];
-//    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSBundle mainBundle] bundleURL]];
-//    [webView loadFileURL:fileUrl allowingReadAccessToURL:[fileUrl URLByDeletingLastPathComponent]];
-    
-    [webView loadRequest:[NSURLRequest requestWithURL:fileUrl]];
-    
+    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSBundle mainBundle] bundleURL]];
+        
 //----------------------------------奇葩情况------------------------------------
     
-    //case4：文件、资源都在蓝色文件夹，但不在同一级目录。
-/*
-    localPath = [[NSBundle mainBundle] pathForResource:@"index3" ofType:@"html" inDirectory:@"HanPi"];
-    NSString *srcPath = [[NSBundle mainBundle] pathForResource:@"2" ofType:@"jpg" inDirectory:@"ReadHtml"];
-    [webView loadFileURL:[NSURL fileURLWithPath:localPath] allowingReadAccessToURL:[NSURL fileURLWithPath:srcPath]];
- */
-
-    //case5：文件在黄，资源在蓝
+    //case3：文件、资源都在蓝色文件夹，但不在同一级目录
+//    localPath = [[NSBundle mainBundle] pathForResource:@"index3" ofType:@"html" inDirectory:@"ReadHtml/HanPi"];
+//    fileUrl = [NSURL fileURLWithPath:localPath];
+////    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSBundle mainBundle] bundleURL]];
+//    NSString *srcPath = [[NSBundle mainBundle] pathForResource:@"2" ofType:@"jpg" inDirectory:@"ReadHtml"];
+//    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSURL fileURLWithPath:srcPath] URLByDeletingLastPathComponent]];
+ 
+    //case4：文件在黄，资源在蓝
+//    localPath = [[NSBundle mainBundle] pathForResource:@"index1" ofType:@"html"];
+//    fileUrl = [NSURL fileURLWithPath:localPath];
+//    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSBundle mainBundle] bundleURL]];
+////    NSString *srcPath = [[NSBundle mainBundle] pathForResource:@"2" ofType:@"jpg" inDirectory:@"ReadHtml"];
+////    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSURL fileURLWithPath:srcPath] URLByDeletingLastPathComponent]];
     
     //case5：文件在蓝，资源在黄
+//    localPath = [[NSBundle mainBundle] pathForResource:@"index2" ofType:@"html" inDirectory:@"ReadHtml"];
+//    fileUrl = [NSURL fileURLWithPath:localPath];
+//    [webView loadFileURL:fileUrl allowingReadAccessToURL:[[NSBundle mainBundle] bundleURL]];
     
-    //这些都无法成功，应该需要修改html中的 相对路径 为 绝对路径，未测试！
+    //这些都无法成功(已验证)，应该需要修改html中图片的 相对路径 为 绝对路径，未测试！
 }
-
-//条件编译
-//loadFileURL（先） 和 loadRequest（后）
-//allowingReadAccessToURL、baseUrl 是html内引用的资源所在的目录地址
 
 @end
